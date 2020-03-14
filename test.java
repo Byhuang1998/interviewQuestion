6d90ee054384a1f6330c486d7048ac7585012251
Byhuang1998 <Byhuang1998@gmail.com>
2020-03-14 23:10:18 +0800

    a test

--- test.java
+++ test.java
@@ -1,18 +1,26 @@
-import java.util.Arrays;
-import java.util.Vector;
+
 public class test {
+	
+	public int maxSubArray(int[] nums) {
+		int max = Integer.MIN_VALUE;
+		int len = nums.length;
+		if (len == 1) return nums[0];
+		for (int i=0; i<len; i++) {
+			int sum = 0;
+			for (int j=i; j<len; j++) {
+				sum += nums[j];
+				if (sum>max) max = sum;
+			}
+		}
+		return max;
+	}
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
-		Vector<String> vector= new Vector<>();
-
-        String[] s = new String[2];
-        s = (String[]) vector.toArray(s);
-        System.out.println(Arrays.toString(s));
-        String[] s1 = new String[2];
-        s1 = (String[]) vector.toArray(s);
-        System.out.println(s == s1);
+		test t = new test();
+		int[] nums = {1,2,3,5,4,-1,-5,7};
+		int ans = t.maxSubArray(nums);
+		System.out.println(ans);
 	}
 
 }
