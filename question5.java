8b26e03c49e6a54073d33bcc8f8b7f6f02cad665
Byhuang1998 <Byhuang1998@gmail.com>
2020-04-14 10:19:15 +0800

    replace the space with %20

---
+++ question5.java
@@ -0,0 +1,13 @@
+
+public class question5 {
+
+	public String replaceSpace(String s) {
+		
+		return s.replaceAll(" ", "%20");
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
