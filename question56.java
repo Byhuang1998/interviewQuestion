74e429306347fee3a1c3a391f2b5f85a2d4ba88d
Byhuang1998 <Byhuang1998@gmail.com>
2020-04-28 13:10:37 +0800

    find single number

--- question56.java
+++ question56.java
@@ -1,32 +1,27 @@
 import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
+
 public class question56 {
     public int[] singleNumbers(int[] nums) {
-        int len = nums.length;
-        Map<Integer, Integer> map = new HashMap<>();
-        for (int i=0; i<len; i++) {
-            if (!map.containsKey(nums[i])) {
-            	map.put(nums[i], 1);
-            } else {
-            	map.put(nums[i], 2);
-            }
-        }
-        int[] pos = new int[2];
-        int i = 0;
-        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
-        	if (i==1 && entry.getValue() == 1) {
-        		pos[i] = entry.getKey();
-        		break;
-        	}
-        	if (i==0 && entry.getValue() == 1) {
-        		pos[i] = entry.getKey();
-        		i ++;
-        	}
-        }
-        return pos;
+    	int ret = 0;
+    	for (int num : nums) {
+    		ret ^= num;
+    	}
+    	int h = 1;
+    	while ((ret & h) == 0) {
+    		h <<= 1;
+    	}
+    	
+    	int a = 0;
+    	int b = 0;
+    	for (int num : nums) {
+    		if ((num & h) == 0) {
+    			a ^= num;
+    		} else {
+    			b ^= num;
+    		}
+    	}
+    	return new int[] {a, b};
     }
-
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		question56 q = new question56();
