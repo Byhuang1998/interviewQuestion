5237ccbf6047f87b986aadb470eeb21757ac6592
Byhuang1998 <Byhuang1998@gmail.com>
2020-05-13 23:15:12 +0800

    找出链表的交点

--- question52.java
+++ question52.java
@@ -15,16 +15,8 @@ public class Solution {
         ListNode l1 = headA;
         ListNode l2 = headB;
         while (l1 != l2) {
-            if (l1 == null) {
-                l1 = headB;
-            } else {
-                l1 = l1.next;
-            }
-            if (l2 == null) {
-                l2 = headA;
-            } else {
-                l2 = l2.next;
-            }
+            l1 = l1 == null ? headB : l1.next;
+            l2 = l2 == null ? headA : l2.next;
         }
         return l1;
     }
