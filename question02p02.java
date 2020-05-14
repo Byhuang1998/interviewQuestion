e9b773363d63586e250946b758b1ce740d4df1da
Byhuang1998 <Byhuang1998@gmail.com>
2020-05-14 12:04:56 +0800

    返回链表倒数第k个节点的值

---
+++ question02p02.java
@@ -0,0 +1,18 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public int kthToLast(ListNode head, int k) {
+        List<Integer> list = new ArrayList<>();
+        while (head != null) {
+            list.add(head.val);
+            head = head.next;
+        }
+        return list.get(list.size() - k);
+    }
+}
