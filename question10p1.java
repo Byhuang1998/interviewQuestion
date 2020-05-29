5b2217e6a555a0ff03e6dc2d012ebcd0ff461db1
Byhuang1998 <Byhuang1998@gmail.com>
2020-05-29 17:40:43 +0800

    菲波那契数列

---
+++ question10p1.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int fib(int n) {
+        if (n < 2) return n;
+        int a = 0, b = 1, sum = 1;
+        for (int i = 2; i <= n; ++i) {
+            sum = (a + b) % 1000000007;
+            a = b;
+            b = sum;
+        }
+        return sum;
+    }
+}
