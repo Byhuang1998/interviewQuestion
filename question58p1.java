90a1cc7ba560572cddf914d945731404b9210e73
Byhuang1998 <Byhuang1998@gmail.com>
2020-06-05 01:11:38 +0800

    翻转字符串

--- question58p1.java
+++ question58p1.java
@@ -1,9 +1,9 @@
 class Solution {
     public String reverseWords(String s) {
-        String[] ans = s.split(" ");
+        // 匹配正则表达式
+        String[] ans = s.split(" +");
         StringBuilder res = new StringBuilder("");
         for (int i = ans.length - 1; i >= 0; --i) {
-            if (ans[i].equals("")) continue;
             res.append(ans[i] + " ");
         }
         return new String(res).trim();
