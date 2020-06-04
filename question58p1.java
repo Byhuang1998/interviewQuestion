e703cb705779d45be6d9b2a03aeb9b2d4c035e8f
Byhuang1998 <Byhuang1998@gmail.com>
2020-06-05 00:47:06 +0800

    翻转字符串

--- question58p1.java
+++ question58p1.java
@@ -1,24 +1,11 @@
 class Solution {
     public String reverseWords(String s) {
-        char[] sc = s.toCharArray();
-        StringBuilder sb = new StringBuilder("");
-        for (int i = sc.length - 1; i >= 0;) {
-            if (sc[i] == ' ') {
-                int j = i;
-                while (j >= 0 && sc[j] == ' ') {
-                    j--;
-                }
-                sb.append(' ');
-                i = j;
-            } else {
-                int j = i;
-                while (j >= 0 && sc[j] != ' ') {
-                    j--;
-                }
-                sb.append(s.substring(j+1, i+1));
-                i = j;
-            }
+        String[] ans = s.split(" ");
+        StringBuilder res = new StringBuilder("");
+        for (int i = ans.length - 1; i >= 0; --i) {
+            if (ans[i].equals("")) continue;
+            res.append(ans[i] + " ");
         }
-        return (new String(sb)).trim();
+        return new String(res).trim();
     }
 }
