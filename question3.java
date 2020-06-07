670499c92196ebb729cbee1ed6eee6d860404f1a
Byhuang1998 <Byhuang1998@gmail.com>
2020-06-08 01:49:40 +0800

    数组中重复出现的数字

--- question3.java
+++ question3.java
@@ -1,10 +1,12 @@
 class Solution {
     public int findRepeatNumber(int[] nums) {
-        Arrays.sort(nums);
-        for (int i = 1; i < nums.length; ++i) {
-            if (nums[i] == nums[i - 1]) {
-                return nums[i];
+        int len = nums.length;
+        int[] ans = new int[len];
+        for (int nn : nums) {
+            if (ans[nn] == 1) {
+                return nn;
             }
+            ans[nn] ++;
         }
         return -1;
     }
